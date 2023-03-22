55cde5e83d61c6727a14aa9c4b71361450821bc3
Carlos Venegas <carlosvenegass@gmail.com>
2023-03-22 18:48:15 -0300

    subiendo base de datos de productos

--- Consultas.js
+++ Consultas.js
@@ -1,6 +1,7 @@
 const {Pool} = require("pg");
 require("dotenv").config({path:"./.env"});
 
+
 const credenciales = {
     host: process.env.HOST,
     user: process.env.USER,
@@ -10,4 +11,17 @@ const credenciales = {
   };
   
   const pool = new Pool(credenciales);
+
+  const obtenerProducto = async ({ limits = 10, page = 0 }) => {
+  try {
+    const offset = (page - 1) * limits
+    const formattedQuery = format('SELECT * FROM productos LIMIT %s OFFSET %s', limits, offset);
+    pool.query(formattedQuery);
+    const { rows: productos } = await pool.query(formattedQuery)
+    console.log(productos)
+
+    return productos
+  } catch { }
+}
+module.exports = { obtenerProducto }
   
